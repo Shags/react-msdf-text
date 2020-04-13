331db235b87063e454ebdcf17f0a0b71c65a2b20
Shags <shags818@hotmail.com>
2020-04-13 12:52:00 -0400

    Updates

---
+++ precache-manifest.c462121e417694503e707071230d2363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7ee3cde86a742b0a323c6395ed5ee6",
+    "url": "/react-msdf-text/index.html"
+  },
+  {
+    "revision": "441516a978ad129c169c",
+    "url": "/react-msdf-text/static/js/2.45cbf3ba.chunk.js"
+  },
+  {
+    "revision": "373eaf0cb5373acfaff29d025176da1f",
+    "url": "/react-msdf-text/static/js/2.45cbf3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fadd7506aea49b69f3",
+    "url": "/react-msdf-text/static/js/main.149aa386.chunk.js"
+  },
+  {
+    "revision": "c7acc33d87ca63541630",
+    "url": "/react-msdf-text/static/js/runtime-main.e2a23e64.js"
+  },
+  {
+    "revision": "ab012798837ed6501f8e65f365bb910a",
+    "url": "/react-msdf-text/static/media/Roboto-Regular~VGPWxChw.ab012798.png"
+  }
+]);
