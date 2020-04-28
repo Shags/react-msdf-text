17d8c31c858fb8dd29d9ade17960305b167d5e36
Shags <shags818@hotmail.com>
2020-04-28 11:35:46 -0400

    Clean up and docs

--- src/Text.jsx
+++ src/Text.jsx
@@ -7,6 +7,7 @@ import {
   Color,
   DoubleSide,
   Quaternion,
+  Matrix4,
 } from 'three'
 import TextGeometry from './TextGeometry'
 import robotoFont from './fonts/roboto/regular.json'
@@ -18,6 +19,8 @@ export const LEFT = 'left'
 export const CENTER = 'center'
 export const BOTTOM = 'bottom'
 export const RIGHT = 'right'
+
+const ZERO = new Vector3(0, 0, 0)
 
 /**
  * Displays the disired text with background and border using the supplied msdf (Multi-Signed Distance Fields) font.
@@ -131,10 +134,18 @@ export const Text = ({
     camera.getWorldPosition(vec)
     return vec
   }, [camera])
-  const cameraDirection = useMemo(() => new Vector3(), [])
-  const cameraToPointProjection = useMemo(() => new Vector3(), [])
-  const worldQuaternion = useMemo(() => new Quaternion(), [])
-  const worldPosition = useMemo(() => new Vector3(), [])
+  const { current: cameraDirection } = useRef(new Vector3())
+  const { current: cameraToPointProjection } = useRef(new Vector3())
+  const { current: worldQuaternion } = useRef(new Quaternion())
+  const { current: worldPosition } = useRef(new Vector3())
+  const { current: modelViewMatrix } = useRef(new Matrix4())
+  const { current: bottomLeftAdj } = useRef(new Vector3())
+  const { current: upperRightAdj } = useRef(new Vector3())
+
+  const { current: rotation } = useRef(new Quaternion())
+  const { current: upright } = useRef(
+    new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), Math.PI)
+  )
 
   const meshRef = useRef()
 
@@ -144,87 +155,105 @@ export const Text = ({
     const self = meshRef.current
     self.parent.getWorldPosition(worldPosition)
     self.parent.getWorldQuaternion(worldQuaternion)
+    // Calculate the model view matrix. The parents is not set initially.
+    modelViewMatrix.multiplyMatrices(
+      camera.matrixWorldInverse,
+      self.parent.matrixWorld
+    )
+    // Initialize the box adjustment values to the screen coordinates
+    bottomLeftAdj.setFromMatrixPosition(modelViewMatrix)
+    upperRightAdj.setFromMatrixPosition(modelViewMatrix)
+
     camera.getWorldPosition(cameraPosition)
     camera.getWorldDirection(cameraDirection)
+
+    // The rotation required to get the text to face the camera. This is different than 'lookAt'.
+    rotation
+      .copy(worldQuaternion)
+      .conjugate()
+      .multiply(camera.quaternion)
+      .multiply(upright)
+    /** Z distance in the camera frame.
+     * Caculate depth of the text's parent in relation to the camera by projecting the
+     * vector of the worlds position to the camera onto the camera's direction vector. */
     cameraToPointProjection
       .copy(worldPosition)
       .sub(cameraPosition)
       .projectOnVector(cameraDirection)
 
-    const box = self.geometry.boundingBox
+    const textBox = self.geometry.boundingBox
     const sphere = self.geometry.boundingSphere
-    const boxSize = box.getSize(new Vector3())
+    const textBoxSize = textBox.getSize(new Vector3())
 
+    // Calculate the offset of the anchor position
     const anchorOffset = {
       x:
         anchorHorz === LEFT
-          ? -box.min.x
+          ? -textBox.min.x
           : anchorHorz === CENTER
           ? -sphere.center.x
           : anchorHorz === RIGHT
-          ? -box.max.x
+          ? -textBox.max.x
           : 0,
       y:
         anchorVert === TOP
-          ? box.min.y
+          ? textBox.min.y
           : anchorVert === CENTER
           ? sphere.center.y
           : anchorVert === BOTTOM
-          ? box.max.y
+          ? textBox.max.y
           : 0,
     }
 
+    // View calculations
     const aspect = size.width / size.height
-    // Calculate the distance from the text parents plane to the camera plane.
-    // We want the distance between these two parellel planes, not the distance from the camera point to the parent point.
-    const distance = Math.abs(cameraToPointProjection.length())
+    const distance = cameraToPointProjection.length() // z distance to point
     const fov = (camera.fov * Math.PI) / 180 // convert vertical fov to radians
     const viewHeight = 2 * Math.tan(fov / 2) * distance // visible
     const viewWidth = viewHeight * aspect
     const factor = size.width / viewWidth
     const scale = fontSize / (factor * font.data.info.size)
 
+    // Calculate the screen position offset
     const placementOffset = {
       x: (viewWidth * positionHorz) / 100 - viewWidth / 2,
       y: viewHeight / 2 - (viewHeight * positionVert) / 100,
     }
 
+    // Use the offsets and scale to deterine the initial text position
     const position = [
       anchorOffset.x * scale + placementOffset.x,
       anchorOffset.y * scale + placementOffset.y,
       0,
     ]
 
-    const upright = new Quaternion().setFromAxisAngle(
-      new Vector3(1, 0, 0),
-      Math.PI
+    // We determine ho much the text box must be moved to keep it visible.
+    // This is the amount of space that would be available around the text box if it was centered on the screen (how much we can move the text before it is off of the screen).
+    const adjustmentSpace = new Vector3(
+      (viewWidth - textBoxSize.x * scale) * 0.5,
+      (viewHeight - textBoxSize.y * scale) * 0.5,
+      0
     )
+    // If the text box is completely on the screen, then the current center of the text will be inside the adjustment space available.
+    // If the text box has moved off the bottom/left side of the screen, then adding the adjustment space to the text box position will result in negative values.
+    bottomLeftAdj.add(adjustmentSpace).min(ZERO)
+    // If the text box has moved off the upper/right side of the screen, then subtracting the text box position from the adjustment space will reslt in negative values.
+    upperRightAdj.multiplyScalar(-1).add(adjustmentSpace).min(ZERO)
+    // We combine these values to get the needed adjustment. Only one of them will contain non zero values, since we can't move off of the top/right and bottom left at the same time, and these will be negative numbers.
+    // The result is either negative postions adjustments for bringing the text back onto the screen from the upper/right, or negative values for the bottom/left.
+    // We zero out the z axis because we are only calculating in 2D space here.
+    const adj = upperRightAdj.clone().sub(bottomLeftAdj).setZ(0)
 
-    const rotation = worldQuaternion
-      .clone()
-      .conjugate()
-      .multiply(camera.quaternion)
-      .multiply(upright)
-
-    const viewBox = new Vector3(viewWidth, viewHeight, 0)
-    const textSize = new Vector3(boxSize.x, boxSize.y, 0).multiplyScalar(scale)
-    const b = viewBox.clone().sub(textSize).multiplyScalar(0.5)
-
-    const zero = new Vector3(0, 0, 0)
-    const a = worldPosition
-      .clone()
-      .applyQuaternion(camera.quaternion.clone().conjugate())
-    const bl = a.clone().add(b).min(zero)
-    const ur = a.clone().multiplyScalar(-1).add(b).min(zero)
-    const adj = ur.clone().sub(bl).setZ(0)
-
+    // The adjument is calculated based on the camera view. We need to rotate these adjustments into the text objects space so they can be correctly applied.
     adj.applyQuaternion(
       worldQuaternion.clone().conjugate().multiply(camera.quaternion)
     )
 
-    const x = new Vector3(...position).add(adj)
-
-    self.position.copy(x)
+    self.position.set(
+      position[0] + adj.x,
+      position[1] + adj.y,
+      position[2] + adj.z
+    )
     self.setRotationFromQuaternion(rotation)
     self.scale.set(scale, scale, scale)
   })
